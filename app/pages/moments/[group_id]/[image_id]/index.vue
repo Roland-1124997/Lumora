189f8ec159201ba164d1930e67b3f9e79e4b03cf
Rottie#7658 <89132@glr.nl>
2025-03-12 19:18:29 +0100

    verbtered het inloaden van de images en een voorbeeld van comments kunnen plaatsen

--- app/pages/moments/[group_id]/[image_id]/index.vue
+++ app/pages/moments/[group_id]/[image_id]/index.vue
@@ -1,25 +1,8 @@
 <template>
-	<splitpanes :horizontal="isMobile" class="w-full h-full" @resize="savePaneSize">
-		<pane :size="paneLeft" class="pl-3 -mt-4 border-l md:pr-3" min-size="50" max-size="60">
-			<div class="hidden w-full gap-2 mb-4 md:flex">
-				<button :disabled="content.author.is_owner" @click="likeImage" class="flex items-center justify-center gap-2 p-2 px-4 text-sm text-white bg-black border border-black rounded-xl">
-					<Icon :name="content.has_liked ? 'ri:heart-fill' : 'ri:heart-line'" size="1.2rem" />
-					{{ content.likes.count }}
-				</button>
-				<button v-if="content?.permision?.delete" @click="deleteData" class="flex items-center justify-center gap-2 p-2 px-4 text-sm text-white bg-black border border-black rounded-xl">
-					<Icon name="ri:delete-bin-5-line" size="1.2rem" />
-				</button>
-				<a target="_parent" download :href="content?.media?.url" class="flex items-center justify-center gap-2 p-2 px-4 text-sm border border-black rounded-xl">Download</a>
-				<button class="flex items-center justify-center w-full gap-2 p-2 px-4 text-sm border border-black rounded-xl">Comment</button>
-			</div>
-			<div class="relative flex items-center justify-center w-full h-full aspect-square min-h-[51vh] max-h-[78vh] md:h-[78vh] overflow-hidden bg-gray-200 border rounded-lg bg-background">
-				<img v-if="loaded" :src="content?.media?.url" class="object-cover w-full h-full" />
-				<icon v-else class="bg-gray-400 animate-spin" name="ri:loader-2-line" size="5em" />
-			</div>
-		</pane>
-		<pane :size="paneRight" class="pl-3 overflow-hidden border-l mb-36 md:mb-auto" >
-			<div class="md:h-[82.5vh] overflow-scroll">
-				<div class="flex w-full gap-2 mt-4 mb-2 md:hidden">
+	<div>
+		<splitpanes :horizontal="isMobile" class="w-full h-full" @resize="savePaneSize">
+			<pane :size="paneLeft" class="pl-3 -mt-4 border-l md:pr-3" min-size="50" max-size="70">
+				<div class="hidden w-full gap-2 mb-4 max-w-[35vw] md:flex">
 					<button :disabled="content.author.is_owner" @click="likeImage" class="flex items-center justify-center gap-2 p-2 px-4 text-sm text-white bg-black border border-black rounded-xl">
 						<Icon :name="content.has_liked ? 'ri:heart-fill' : 'ri:heart-line'" size="1.2rem" />
 						{{ content.likes.count }}
@@ -27,45 +10,72 @@
 					<button v-if="content?.permision?.delete" @click="deleteData" class="flex items-center justify-center gap-2 p-2 px-4 text-sm text-white bg-black border border-black rounded-xl">
 						<Icon name="ri:delete-bin-5-line" size="1.2rem" />
 					</button>
+					<a target="_parent" download :href="content?.media?.url" class="flex items-center justify-center gap-2 p-2 px-4 text-sm border border-black rounded-xl">Download</a>
+					<button @click="focusEditable" class="flex items-center justify-center w-full gap-2 p-2 px-4 text-sm border border-black rounded-xl">Comment</button>
+				</div>
+				<div class="relative flex items-center justify-center w-full h-full aspect-square min-h-[51vh] max-h-[78vh] md:h-[78vh] overflow-hidden bg-gray-200 border rounded-lg bg-background">
+					<img v-if="loaded" :src="content?.media?.url" class="object-cover w-full h-full" />
+					<icon v-else class="bg-gray-400 animate-spin" name="ri:loader-2-line" size="5em" />
+				</div>
+			</pane>
+			<pane :size="paneRight" class="pl-3 overflow-hidden border-l mb-36 md:mb-auto">
+				<div class="md:h-[82.5vh] overflow-scroll">
+					<div class="flex w-full gap-2 mt-4 mb-2 md:hidden">
+						<button :disabled="content.author.is_owner" @click="likeImage" class="flex items-center justify-center gap-2 p-2 px-4 text-sm text-white bg-black border border-black rounded-xl">
+							<Icon :name="content.has_liked ? 'ri:heart-fill' : 'ri:heart-line'" size="1.2rem" />
+							{{ content.likes.count }}
+						</button>
+						<button v-if="content?.permision?.delete" @click="deleteData" class="flex items-center justify-center gap-2 p-2 px-4 text-sm text-white bg-black border border-black rounded-xl">
+							<Icon name="ri:delete-bin-5-line" size="1.2rem" />
+						</button>
 
-					<a v-if="!PWAInstalled" target="_parent" download :href="content?.media?.url" class="flex items-center justify-center gap-2 p-2 px-4 text-sm border border-black rounded-xl">Download</a>
-					<div v-else class="relative group">
-						<button disabled class="flex items-center justify-center gap-2 p-2 px-4 text-sm border border-black disabled:bg-gray-200 rounded-xl">Download</button>
-						<div class="absolute z-50 hidden px-4 py-1 text-xs text-center text-white transition duration-200 ease-in-out transform -translate-y-full bg-black rounded-md w-44 top-[6.35rem] group-hover:block">
-							<p class="text-center p-[0.45em]">
-								<span class="font-semibold text-balance"> Currently not supported on PWAs! </span>
-							</p>
-							<div class="absolute w-3 h-3 bg-black transform rotate-45 -top-[0.44em] left-5 -translate-x-1/2"></div>
+						<a v-if="!PWAInstalled" target="_parent" download :href="content?.media?.url" class="flex items-center justify-center gap-2 p-2 px-4 text-sm border border-black rounded-xl">Download</a>
+						<div v-else class="relative group">
+							<button disabled class="flex items-center justify-center gap-2 p-2 px-4 text-sm border border-black disabled:bg-gray-200 rounded-xl">Download</button>
+							<div class="absolute z-50 hidden px-4 py-1 text-xs text-center text-white transition duration-200 ease-in-out transform -translate-y-full bg-black rounded-md w-44 top-[6.35rem] group-hover:block">
+								<p class="text-center p-[0.45em]">
+									<span class="font-semibold text-balance"> Currently not supported on PWAs! </span>
+								</p>
+								<div class="absolute w-3 h-3 bg-black transform rotate-45 -top-[0.44em] left-5 -translate-x-1/2"></div>
+							</div>
+						</div>
+						<button @click="focusEditable" class="flex items-center justify-center w-full gap-2 p-2 px-4 text-sm border border-black rounded-xl">Comment</button>
+					</div>
+
+					<hr class="mt-4 mb-2 md:hidden" />
+					<h2 class="mb-4 text-xl font-semibold">You might also like</h2>
+					<p class="mb-2 -mt-4">Explore more posts from this author</p>
+
+					<div :class="paneRight <= 38.5 ? 'grid-cols-2' : 'grid-cols-3'" class="grid gap-2 mt-4">
+						<div v-for="content in content.more_from_author" class="flex-shrink-0 w-full h-full overflow-hidden bg-gray-200 border rounded-md aspect-square snap-start">
+							<NuxtLink v-if="loaded" :to="`/moments/${group_id}/${content.post_data.id}`">
+								<img :src="content.post_data?.media?.url" class="object-cover w-full h-full aspect-square" />
+							</NuxtLink>
+							<div v-else class="flex items-center justify-center flex-shrink-0 w-full h-full overflow-hidden bg-gray-200 border rounded-md aspect-square snap-start">
+								<icon class="bg-gray-400 animate-spin" name="ri:loader-2-line" size="2em" />
+							</div>
+						</div>
+
+						<div v-for="content in 6 - content.more_from_author.length" class="flex items-center justify-center flex-shrink-0 w-full h-full overflow-hidden bg-gray-200 border rounded-md aspect-square snap-start">
+							<icon class="bg-gray-400 animate-spin" name="ri:loader-2-line" size="2em" />
 						</div>
 					</div>
-					<button class="flex items-center justify-center w-full gap-2 p-2 px-4 text-sm border border-black rounded-xl">Comment</button>
+
+					<hr class="my-2 mt-4" />
+
+					<h2 class="mb-3 text-xl font-semibold">Comments</h2>
+					<form @submit.prevent="handleSumbitComments()">
+						<div class="flex items-center justify-center gap-2 ">
+							<textarea v-model="comment" placeholder="Leave an message" ref="editable" class="w-full resize-none p-4 bg-gray-100 rounded-xl outline-none appearance-none h-[3.5rem] focus:border focus:border-black"></textarea>
+							<button class="flex items-center justify-center p-[0.85rem] text-sm text-white bg-black/90 rounded-xl">
+								<Icon name="ri:send-plane-fill" size="1.5rem"/>
+							</button>
+						</div>
+					</form>
 				</div>
-
-				<hr class="mt-4 mb-2 md:hidden" />
-				<h2 class="mb-4 text-xl font-semibold">You might also like</h2>
-				<p class="mb-2 -mt-4">Explore more posts from this author</p>
-
-				<div class="grid grid-cols-3 gap-2 mt-4">
-					<NuxtLink v-if="loaded" :to="`/moments/${group_id}/${content.post_data.id}`" v-for="content in content.more_from_author" class="flex-shrink-0 overflow-hidden bg-gray-200 border rounded-md aspect-square w-fit h-fit snap-start">
-						<img :src="content.post_data?.media?.url" class="object-cover w-full h-full aspect-square" />
-					</NuxtLink>
-
-					<div v-else v-for="content in content.more_from_author" class="flex items-center justify-center flex-shrink-0 w-full h-full overflow-hidden bg-gray-200 border rounded-md aspect-square snap-start">
-						<icon class="bg-gray-400 animate-spin" name="ri:loader-2-line" size="2em" />
-					</div>
-
-					<div v-for="content in 6 - content.more_from_author.length" class="flex items-center justify-center flex-shrink-0 w-full h-full overflow-hidden bg-gray-200 border rounded-md aspect-square snap-start">
-						<icon class="bg-gray-400 animate-spin" name="ri:loader-2-line" size="2em" />
-					</div>
-				</div>
-
-				<hr class="my-2 mt-4" />
-
-				<h2 class="mb-3 text-xl font-semibold">Comments</h2>
-				<p class="p-4 mb-2 bg-gray-100 rounded-md">Leave a message</p>
-			</div>
-		</pane>
-	</splitpanes>
+			</pane>
+		</splitpanes>
+	</div>
 </template>
 
 <script setup langs="ts">
@@ -94,6 +104,29 @@
 	definePageMeta({
 		middleware: "unauthorized",
 	});
+
+	/*
+	 ************************************************************************************
+	 */
+
+	const editableDiv = templateRef("editable");
+
+	const focusEditable = () => {
+		editableDiv.value.focus();
+	};
+
+
+	const comment = ref()
+	const handleSumbitComments = () => {
+		comment.value = null
+	}
+
+	
+
+
+
+
+
 
 	/*
 	 ************************************************************************************
@@ -223,45 +256,35 @@
 	 */
 
 	const isMobile = ref(window.innerWidth < 768);
-	const paneLeft = ref(45);
+	const paneLeft = ref(60);
 	const paneRight = ref(60);
 
 	const updateScreenSize = () => {
 		isMobile.value = window.innerWidth < 768;
 	};
 
-	onMounted(() => {
-		window.addEventListener("resize", updateScreenSize);
-	});
+	window.addEventListener("resize", updateScreenSize);
 
 	onUnmounted(() => {
 		window.removeEventListener("resize", updateScreenSize);
 	});
 
-	onMounted(() => {
-		const panes = JSON.parse(sessionStorage.getItem(`panes_size`));
+	const panes = JSON.parse(sessionStorage.getItem(`panes_size`));
 
-		if(panes) {
-			paneLeft.value = panes[0].size
-			paneRight.value = panes[1].size
-		}
+	if (panes) {
+		paneLeft.value = panes[0].size;
+		paneRight.value = panes[1].size;
+	}
 
-	});
-
-	
 	const savePaneSize = (event) => {
 		sessionStorage.setItem(`panes_size`, JSON.stringify(event.panes));
-		paneLeft.value = event.panes[0].size
-		paneRight.value = event.panes[1].size
+		paneLeft.value = event.panes[0].size;
+		paneRight.value = event.panes[1].size;
 	};
 </script>
 
 <style>
 	.splitpanes--vertical > .splitpanes__splitter {
-		@apply w-1 rounded-full bg-gray-200 hover:bg-gray-300;
-	}
-
-	.splitpanes--horizontal > .splitpanes__splitter {
-		@apply w-1 rounded-full bg-gray-200 hover:bg-gray-300;
+		@apply w-1 h-[82.5vh] rounded-full mr-3 bg-gray-200 hover:bg-gray-300;
 	}
 </style>
