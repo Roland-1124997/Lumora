114d7cba6c14b7d6329ce4e579aeea21ba268353
Rottie#7658 <89132@glr.nl>
2025-03-18 22:44:23 +0100

    updated

--- server/api/moments/settings/[group_id]/index.get.ts
+++ server/api/moments/settings/[group_id]/index.get.ts
@@ -50,7 +50,9 @@ export default defineSupabaseEventHandler(async (event, user, client, server) =>
             description: data.description,
             last_active: data.last_active,
             permision: {
-                delete: permissions?.can_delete_group || permissions?.user_id === data.author_id,
+                change: data.owner_id === user.id,
+                delete: permissions?.can_delete_messages_all || permissions?.user_id === data.author_id,
+                create: permissions?.can_edit_group || settings?.everyone_can_create_link || false,
                 edit: permissions?.can_edit_group
             },
             media: {
