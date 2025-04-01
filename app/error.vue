8f096e8f4f27e778cf5c3d57bc72091ff90d8ce1
Rottie#7658 <120731496+Rottie89132@users.noreply.github.com>
2025-04-01 15:56:51 +0200

    Update error.vue

--- app/error.vue
+++ app/error.vue
@@ -38,7 +38,7 @@
 						</div>
 
 						<div class="flex flex-col justify-center gap-3 pt-2 sm:flex-row">
-							<button @click="handleError('/home')" class="flex items-center gap-2 px-4 py-2 font-medium text-white rounded-lg bg-[#756145]/80 hover:bg-[#756145] btn-primary">
+							<button @click="handleError('/')" class="flex items-center gap-2 px-4 py-2 font-medium text-white rounded-lg bg-[#756145]/80 hover:bg-[#756145] btn-primary">
 								<Icon name="mdi:home" class="w-4 h-4" />
 								Back to Home
 							</button>
