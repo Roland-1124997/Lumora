5da81f46eaa40595f91cf5de092adb7d49a2e3e9
Rottie#7658 <120731496+Rottie89132@users.noreply.github.com>
2025-05-16 07:51:28 +0200

    Update redirect.client.ts

--- app/middleware/redirect.client.ts
+++ app/middleware/redirect.client.ts
@@ -1,8 +1,10 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
 
     const store = useSessionsStore()
-    const { error } = await store.getSession()
+    const { data, error } = await store.getSession()
+    
     if (error) return navigateTo("/auth")
+    if (data && data.mfa_needs_to_verfied) return navigateTo("/auth")
 
     else return navigateTo("/moments")
     
